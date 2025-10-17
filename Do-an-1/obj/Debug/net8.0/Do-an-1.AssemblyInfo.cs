a97bd292a49feb574b2ba15d11143499050b038b
XuanHoan207 <hoanlinhk2@gmail.com>
2025-10-17 16:12:53 +0700

    Fix .vs permission issue and add .gitignore

--- Do-an-1/obj/Debug/net8.0/Do-an-1.AssemblyInfo.cs
+++ Do-an-1/obj/Debug/net8.0/Do-an-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do-an-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97afaf54117d7b2a586e1ea7cdee75898a5c38a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do-an-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do-an-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
